3f0e74bb2778748d2cf22de0a41bd9d46d9a3238
KrisKioko <chris.kioko20@gmail.com>
2023-11-29 13:07:20 +0300

    Final CountConfortingBitMasks

--- src/App.java
+++ src/App.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 
 class Solution {
     public static int zeros(int N) {
+        // This variable will be used to keep count of the bits in the binary representation of 'N'
         int conformingIntegers = 0;
 
         for (int i = 0; i < 30; i++) {
+            // Checks if the least significant bit is '0' and if so it is increased by 1
             if (N % 2 == 0) {
                 conformingIntegers++;
             }
-            N >>= 1;
+            N >>= 1;    // The value of 'N' is right-shifted by '1' effectively moving to the next bit
         }
-        return conformingIntegers;
+        return conformingIntegers;    // The method returns the count of zero bits in the binary representation of integer 'N'
     }
 
+    // This method takes integer 'N' as a parameter and returns '2^zeros(N)'
     public static int countConformingIntegers(int N) {
-        return 1 << zeros(N);
+        return 1 << zeros(N);    // This line uses the left-shift operator to calculate '2^zeros(N)'
     }
 
     public static int solution(int A, int B, int C) {
